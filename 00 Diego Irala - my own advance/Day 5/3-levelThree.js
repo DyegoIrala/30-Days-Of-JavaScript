b48b1531359e1451d364d5d0fa3117e03d669fe8
Diego <diego_irala1198@outlook.com>
2025-02-14 18:40:17 -0400

    Final Day 5 excercices

--- 00 Diego Irala - my own advance/Day 5/3-levelThree.js
+++ 00 Diego Irala - my own advance/Day 5/3-levelThree.js
@@ -17,17 +17,83 @@ const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24,25]
     console.log(newArray)
     //Find the average age(all items divided by number of items)
     let avgValue = 0
-    function suma(array){
+    function average(array){
         for(i = 0;i<array.length;i++){
             avgValue=avgValue+array[i]}
         avgValue = avgValue/array.length
         console.log(avgValue)
+        return avgValue
     }
 
-    suma(ages)
+    average(ages)
 
     //Find the range of the ages(max minus min)
     console.log(Math.max(...ages))
     console.log(Math.min(...ages))
 
-    //Compare the value of (min - average) and (max - average), use abs() method 1.Slice the first ten countries from the countries array
+    //Compare the value of (min - average) and (max - average), use abs() 
+    // method 1.Slice the first ten countries from the countries array
+    let avgValueTwo = average(ages)
+    console.log(Math.max(...ages)== avgValue)
+    console.log(Math.min(...ages)== avgValue)
+
+    const countries = [
+        'Albania',
+        'Bolivia',
+        'Canada',
+        'Denmark',
+        'Ethiopia',
+        'Finland',
+        'Germany',
+        'Hungary',
+        'Ireland',
+        'Japan',
+        'Kenya'
+      ]
+
+    let tenCountries = countries.slice(0,10)
+    console.log(tenCountries)
+
+     //2 Find the middle country(ies) in the countries array
+     console.log(countries)
+     console.log(countries.length)
+     let middleCountries = []
+     if(countries.length%2 ==1){
+        let starIndex = Math.floor((countries.length-1)/2)
+        let endIndex = starIndex +1
+        middleCountries = countries.slice(starIndex,endIndex)
+        
+        
+     }
+     else{
+        let starIndex = Math.floor((countries.length-1)/2)
+        let endIndex = starIndex +2
+        middleCountries = countries.slice(starIndex,endIndex)
+                       
+     }
+
+     console.log(middleCountries)
+
+     //3 Divide the countries array into two equal arrays if it is even. 
+     // If countries array is not even , one more country for the first half.
+
+     let countriesOne
+     let countriesTwo
+    //  countries.push("delete")
+     console.log(countries.length)
+
+     if(countries.length%2 ==0){
+        endIndex=Math.floor((countries.length-1)/2)+1
+        countriesOne = countries.slice(0,endIndex)
+        countriesTwo = countries.slice(endIndex,countries.length)
+
+     }
+     else{
+        endIndex=Math.floor((countries.length-1)/2)+1
+        countriesOne = countries.slice(0,endIndex)
+        countriesTwo = countries.slice(endIndex,countries.length)
+     }
+
+     console.log(countriesOne)
+     console.log(countriesTwo)
+     
